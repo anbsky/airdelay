321077701dad07f32c94ae3874dae2430ccc8ee9
Andrey Beletsky <ecne@vr2.net>
2013-08-24 00:51:14 +0400

    async tornado runner

--- airparse/tornado_runner.py
+++ airparse/tornado_runner.py
@@ -15,65 +15,43 @@ from tornado.options import define, options
 import tornado.gen
 import functools
 
-from parsers import registry, TimetableRow
+from parsers import parsers
 
 define("port", default=8000, help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    @tornado.gen.engine
+    @tornado.gen.coroutine
     def get(self, iata_code, _type=None):
         client = tornado.httpclient.AsyncHTTPClient()
-#        client.fetch("http://search.twitter.com/search.json?" +\
-#                     urllib.urlencode({"q": query, "result_type": "recent", "rpp": 100}),
         try:
-            crawler = registry.get(iata_code)
+            parser = parsers.initialize(iata_code)
         except KeyError:
             self.write(json.dumps({'status': 'error', 'message': 'no such airport found'}))
             self.finish()
         else:
-            response = yield tornado.gen.Task(client.fetch, crawler.url)
-            self.content_type = 'application/json'
-            flights = []
-            our_response = {
-                'status': 'success',
-                'iata_code': iata_code,
-                'flights': flights
-            }
-            for record in crawler.process(response.body):
-                flights.append(record)
-            self.write(json.dumps(our_response, default=TimetableRow.json_handler))
+            records = []
+            if parser.is_multi_url:
+                for _type, url in parser.url.items():
+                    records += list(parser.parse(parser.parse_html((
+                        yield tornado.gen.Task(client.fetch, url, headers=parser.get_request_headers())
+                    )), type=_type))
+                parser.sleep()
+            else:
+                records = list(parser.parse(parser.parse_html((
+                    yield tornado.gen.Task(client.fetch, parser.url, headers=parser.get_request_headers())
+                )), type=_type))
+
+            self.write(parser.to_json(records))
+            self.set_header('Content-Type', 'application/json')
             self.finish()
-
-
-class SyncIndexHandler(tornado.web.RequestHandler):
-    def get(self, iata_code, _type=None):
-        client = tornado.httpclient.HTTPClient()
-        try:
-            crawler = registry.get(iata_code)
-        except KeyError:
-            self.write(json.dumps({'status': 'error', 'message': 'no such airport found'}))
-        else:
-            response = client.fetch(crawler.url)
-            self.content_type = 'application/json'
-            flights = []
-            our_response = {
-                'status': 'success',
-                'iata_code': iata_code,
-                'flights': flights
-            }
-            for record in crawler.process(response.body):
-                flights.append(record)
-            self.write(json.dumps(our_response, default=TimetableRow.json_handler))
 
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application(handlers=[
         (r'/airports/(.+?)/(?:(.+?)/)?$', IndexHandler),
-        (r'/airports/(.+?)/(?:(.+?)/)?sync/$', SyncIndexHandler),
-    ])
+    ], debug=True)
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
